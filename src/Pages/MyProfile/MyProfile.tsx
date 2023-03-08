4bf6df31714b1e44b314067975d155fa81078f7c
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2023-03-08 14:55:53 -0300

    Termiando a parte logica da PageMyProfile

--- src/Pages/MyProfile/MyProfile.tsx
+++ src/Pages/MyProfile/MyProfile.tsx
@@ -14,53 +14,17 @@ export const MyProfile = () => {
     const [dataUser, setDataUser] = useState<IUser | number>(0);
     const [numberId, setNumberId] = useState<number>(0);
 
-    useEffect(() => {
+    
+    useEffect(()=>{
+        userLogin;
+        const {id} =  user as IUser;
+        console.log(id);
+    },[]);
 
-        const getDatasUser = async () => {
-            const { id } = { ...user };
+    useEffect(()=>{
+        console.log(user);
+    },[user]);
 
-            if (!id == undefined) {
-                try {
-                    setNumberId(id as number)
-                    const token = localStorage.getItem('@TOKEN') as string;
-                    console.log(token);
-                    const response = await api.get(`/users/${id}`, {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        }
-                    })
-                    console.log(response);
-                    setDataUser(response.data)
-                } catch (error) {
-                    
-                }
-            }
-        }
-        getDatasUser();
-    }, [userLogin]);
-
-    useEffect(() => {
-        const renderInfoUser = async () => {
-            console.log(numberId);
-            try {
-                const token = localStorage.getItem('@TOKEN') as string;
-                console.log(token);
-                const response = await api.get(`/users/${numberId}`, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                })
-                console.log(response);
-                setDataUser(response.data)
-            }
-
-             catch (error) {
-                console.log(error)
-            }
-        }
-            renderInfoUser();
-
-        }, [numberId])
     return (
         <>
             <h1>Olá </h1>
